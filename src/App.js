53e1b79722eb5b562dda63e15c3b5edef5549bc6
Akira Watson-Brown <akirawatson-brown@new-host-8.home>
2023-04-27 12:23:06 -0400

    add env

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Simple react</div>;
+  console.log(process.env);
+
+  return (
+    <div className="App">
+      Simple react
+      <div>
+        Current API{" "}
+        {process.env.NODE_ENV === "development"
+          ? process.env.REACT_APP_DEVELOPMENT_API_KEY
+          : process.env.REACT_APP_PRODUCTION_API_KEY}
+      </div>
+    </div>
+  );
 }
 
 export default App;
